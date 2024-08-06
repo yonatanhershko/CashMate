08ebb3de1bd3aa7c81f9a2b758e967fd6684ccb7
Yonatan Hershko <yonher8@gmail.com>
2024-08-06 14:03:23 +0300

    Add demo data

--- src/services/wallet/index.js
+++ src/services/wallet/index.js
@@ -6,21 +6,33 @@ import { walletService as local } from './wallet.service.local'
 import { walletService as remote } from './wallet.service.remote'
 
 function getEmptyWallet() {
-	return {
+    return {
         title: '',
-        isStarred: false,
+        description: '',
+        balance: 0,
+        currency: 'ILS',
         archivedAt: null,
-        createdBy: {},
-        style: {
-            backgroundImage: 'https://images.unsplash.com/photo-1480497490787-505ec076689f?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        createdBy: {
+            id: '',
+            name: '',
+            img: ''
         },
-        labels: [],
+        style: {
+            mode: 'dark',
+            darkMode: {
+                backgroundColor: '#1a1a1a',
+                textColor: '#ffffff',
+            },
+            lightMode: {
+                backgroundColor: '#ffffff',
+                textColor: '#000000',
+            }
+        },
         members: [],
-        groups: [],
         activities: [],
-        cmpsOrder: [],
+        isLocked: true
     }
-    
+
 }
 
 
@@ -37,41 +49,41 @@ if (DEV) window.walletService = walletService
 
 
 // wallet
-// const wallet = {
-//     id: 'unique-id-123',
-//     balance: 0,
-//     currency: 'ILS',
-//     style: {
-//         mode: 'dark',
-//         darkMode: {
-//             backgroundColor: '#1a1a1a',
-//             textColor: '#ffffff',
-//         },
-//         lightMode: {
-//             backgroundColor: '#ffffff',
-//             textColor: '#000000',
-//         }
-//     },
-//     activities: [
-//         // Example activity
-//         {
-//             id: 'activity-1',
-//             type: 'add',
-//             amount: 100,
-//             date: new Date('2024-08-06T10:00:00'),
-//             description: 'Salary deposit'
-//         },
-//     ],
-//     members: [
-//         // Example member
-//         {
-//             id: 'member-1',
-//             name: 'John Doe',
-//             img:''
-//         },
-//     ],
-//     createdBy: 'creator-id',
-//     title: 'My Wallet',
-//     description: '',
-//     isLocked: true,
-// }
+const wallet = {
+    id: 'unique-id-123',
+    balance: 0,
+    currency: 'ILS',
+    style: {
+        mode: 'dark',
+        darkMode: {
+            backgroundColor: '#1a1a1a',
+            textColor: '#ffffff',
+        },
+        lightMode: {
+            backgroundColor: '#ffffff',
+            textColor: '#000000',
+        }
+    },
+    activities: [
+        // Example activity
+        {
+            id: 'activity-1',
+            type: 'add',
+            amount: 100,
+            date: new Date('2024-08-06T10:00:00'),
+            description: 'Salary deposit'
+        },
+    ],
+    members: [
+        // Example member
+        {
+            id: 'member-1',
+            name: 'John Doe',
+            imgUrl:''
+        },
+    ],
+    createdBy: 'creator-id',
+    title: 'My Wallet',
+    description: '',
+    isLocked: true,
+}
--- src/services/wallet/wallet.service.local.js
+++ src/services/wallet/wallet.service.local.js
@@ -4,327 +4,129 @@ import { loadFromStorage, makeId, saveToStorage } from '../util.service'
 
 const STORAGE_KEY = 'wallet'
 
-_createBoards()
+_createWallet()
 
 export const walletService = {
     query,
     getById,
     save,
     remove,
-    addBoardMsg
+    addWalletMsg
 }
 
 window.cs = walletService
 
 
-async function query(filterBy = { name: '' }) {
-    var boards = await storageService.query(STORAGE_KEY)
+async function query() {
+    var wallets = await storageService.query(STORAGE_KEY)
 
-    return boards
+    return wallets
 }
 
-function getById(boardId) {
-    return storageService.get(STORAGE_KEY, boardId)
+function getById(walletId) {
+    return storageService.get(STORAGE_KEY, walletId)
 }
 
-async function remove(boardId) {
+async function remove(walletId) {
     // throw new Error('Nope')
-    await storageService.remove(STORAGE_KEY, boardId)
+    await storageService.remove(STORAGE_KEY, walletId)
 }
 
-async function save(board) {
-    var savedBoard
-    if (board._id) {
-        const boardToSave = {
-            _id: board._id,
-            name: board.name
+async function save(wallet) {
+    var savedWallet
+    if (wallet._id) {
+        const walletToSave = {
+            _id: wallet._id,
+            name: wallet.name
         }
-        savedBoard = await storageService.put(STORAGE_KEY, boardToSave)
+        savedWallet = await storageService.put(STORAGE_KEY, walletToSave)
     } else {
-        const boardToSave = {
-            name: board.name
+        const walletToSave = {
+            name: wallet.name
         }
-        savedBoard = await storageService.post(STORAGE_KEY, boardToSave)
+        savedWallet = await storageService.post(STORAGE_KEY, walletToSave)
     }
-    return savedBoard
+    return savedWallet
 }
 
-async function addBoardMsg(boardId, txt) {
+async function addWalletMsg(walletId, txt) {
     // Later, this is all done by the backend
-    const board = await getById(boardId)
+    const wallet = await getById(walletId)
 
     const msg = {
         id: makeId(),
         txt
     }
-    board.msgs.push(msg)
-    await storageService.put(STORAGE_KEY, board)
+    wallet.msgs.push(msg)
+    await storageService.put(STORAGE_KEY, wallet)
 
     return msg
 }
 
-async function _createBoards() {
-    let boards = await loadFromStorage(STORAGE_KEY)
-    if (boards && boards.length !== 0) return
-    boards = [
+async function _createWallet() {
+    let wallets = await loadFromStorage(STORAGE_KEY)
+    if (wallets && wallets.length !== 0) return
+    wallets = [
         {
             _id: makeId(),
-            title: 'Demo 2',
-            isStarred: false,
+            title: 'Yona Wallet',
+            description: 'A demo wallet with dark mode',
+            balance: 1500,
+            currency: 'ILS',
             archivedAt: null,
             createdBy: {
-                _id: 'u101',
-                fullname: 'Dolev Levy',
-                imgUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+                id: 'u101',
+                name: 'John Doe',
+                img: 'https://example.com/john-doe-profile.jpg'
             },
             style: {
-                backgroundImage: 'https://images.unsplash.com/photo-1480497490787-505ec076689f?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                mode: 'dark',
+                darkMode: {
+                    backgroundColor: '#1a1a1a',
+                    textColor: '#ffffff',
+                },
+                darkMode: {
+                    backgroundColor: '#1a1a1a',
+                    textColor: '#ffffff',
+                }
             },
-            labels: [
-                {
-                    id: 'l101',
-                    title: 'Done',
-                    color: '#61bd4f',
-                },
-                {
-                    id: 'l102',
-                    title: 'Progress',
-                    color: '#61bd33',
-                },
-            ],
             members: [
                 {
-                    _id: 'u101',
-                    fullname: 'Dolev Levy',
-                    imgUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+                    id: 'u101',
+                    name: 'John Doe',
+                    img: 'https://example.com/john-doe-profile.jpg'
+                },
+                {
+                    id: 'u102',
+                    name: 'Jane Smith',
+                    img: 'https://example.com/jane-smith-profile.jpg'
                 }
-            ],
-            groups: [
-                {
-                    id: 'g101',
-                    title: 'Group 1',
-                    archivedAt: 1589983468418,
-                    tasks: [
-                        {
-                            id: 'c101',
-                            title: 'Replace logo',
-                        },
-                        {
-                            id: 'c102',
-                            title: 'Add Samples',
-                        },
-                    ],
-                    style: {},
-                },
-                {
-                    id: 'g102',
-                    title: 'Group 2',
-                    tasks: [
-                        {
-                            id: 'c103',
-                            title: 'Do that',
-                            archivedAt: 1589983468418,
-                        },
-                        {
-                            id: 'c104',
-                            title: 'Help me',
-                            status: 'inProgress', // monday / both
-                            priority: 'high', // monday / both
-                            dueDate: '2024-09-24',
-                            description: 'description',
-                            comments: [
-                                {
-                                    id: 'ZdPnm',
-                                    title: 'also @yaronb please CR this',
-                                    createdAt: 1590999817436,
-                                    byMember: {
-                                        _id: 'u101',
-                                        fullname: 'Tal Tarablus',
-                                        imgUrl: '',
-                                    },
-                                },
-                            ],
-                            checklists: [
-                                {
-                                    id: 'YEhmF',
-                                    title: 'Checklist',
-                                    todos: [
-                                        {
-                                            id: '212jX',
-                                            title: 'To Do 1',
-                                            isDone: false,
-                                        },
-                                    ],
-                                },
-                            ],
-                            memberIds: ['u101'],
-                            labelIds: ['l101', 'l102'],
-                            byMember: {
-                                _id: 'u101',
-                                fullname: 'Tal Tarablus',
-                                imgUrl: '',
-                            },
-                            style: {
-                                backgroundColor: '#26de81',
-                            },
-                        },
-                    ],
-                    style: {},
-                },
             ],
             activities: [
                 {
                     id: 'a101',
-                    title: 'Changed Color',
-                    createdAt: 154514,
-                    byMember: {
-                        _id: 'u101',
-                        fullname: 'Abi Abambi',
-                        imgUrl: 'http://some-img',
-                    },
-                    group: {
-                        id: 'g101',
-                        title: 'Urgent Stuff',
-                    },
-                    task: {
-                        id: 'c101',
-                        title: 'Replace Logo',
-                    },
-                },
-            ],
-
-            // For Monday draggable columns (optional)
-            cmpsOrder: ['StatusPicker', 'MemberPicker', 'DatePicker'],
-        },
-        {
-            _id: makeId(),
-            title: 'Demo 1',
-            isStarred: false,
-            archivedAt: null,
-            createdBy: {
-                _id: 'u101',
-                fullname: 'Dolev Levy',
-                imgUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-            },
-            style: {
-                backgroundImage: 'https://images.unsplash.com/photo-1480497490787-505ec076689f?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            },
-            labels: [
-                {
-                    id: 'l101',
-                    title: 'Done',
-                    color: '#61bd4f',
+                    type: 'add',
+                    amount: 1000,
+                    date: Date.now() - 86400000, // Yesterday
+                    description: 'Initial deposit'
                 },
                 {
-                    id: 'l102',
-                    title: 'Progress',
-                    color: '#61bd33',
+                    id: 'a102',
+                    type: 'add',
+                    amount: 700,
+                    date: Date.now() - 3600000, // 1 hour ago
+                    description: 'Salary'
                 },
-            ],
-            members: [
                 {
-                    _id: 'u101',
-                    fullname: 'Dolev Levy',
-                    imgUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+                    id: 'a103',
+                    type: 'remove',
+                    amount: 200,
+                    date: Date.now(), // Now
+                    description: 'Groceries'
                 }
             ],
-            groups: [
-                {
-                    id: 'g101',
-                    title: 'Group 1',
-                    archivedAt: 1589983468418,
-                    tasks: [
-                        {
-                            id: 'c101',
-                            title: 'Replace logo',
-                        },
-                        {
-                            id: 'c102',
-                            title: 'Add Samples',
-                        },
-                    ],
-                    style: {},
-                },
-                {
-                    id: 'g102',
-                    title: 'Group 2',
-                    tasks: [
-                        {
-                            id: 'c103',
-                            title: 'Do that',
-                            archivedAt: 1589983468418,
-                        },
-                        {
-                            id: 'c104',
-                            title: 'Help me',
-                            status: 'inProgress', // monday / both
-                            priority: 'high', // monday / both
-                            dueDate: '2024-09-24',
-                            description: 'description',
-                            comments: [
-                                {
-                                    id: 'ZdPnm',
-                                    title: 'also @yaronb please CR this',
-                                    createdAt: 1590999817436,
-                                    byMember: {
-                                        _id: 'u101',
-                                        fullname: 'Tal Tarablus',
-                                        imgUrl: '',
-                                    },
-                                },
-                            ],
-                            checklists: [
-                                {
-                                    id: 'YEhmF',
-                                    title: 'Checklist',
-                                    todos: [
-                                        {
-                                            id: '212jX',
-                                            title: 'To Do 1',
-                                            isDone: false,
-                                        },
-                                    ],
-                                },
-                            ],
-                            memberIds: ['u101'],
-                            labelIds: ['l101', 'l102'],
-                            byMember: {
-                                _id: 'u101',
-                                fullname: 'Tal Tarablus',
-                                imgUrl: '',
-                            },
-                            style: {
-                                backgroundColor: '#26de81',
-                            },
-                        },
-                    ],
-                    style: {},
-                },
-            ],
-            activities: [
-                {
-                    id: 'a101',
-                    title: 'Changed Color',
-                    createdAt: 154514,
-                    byMember: {
-                        _id: 'u101',
-                        fullname: 'Abi Abambi',
-                        imgUrl: 'http://some-img',
-                    },
-                    group: {
-                        id: 'g101',
-                        title: 'Urgent Stuff',
-                    },
-                    task: {
-                        id: 'c101',
-                        title: 'Replace Logo',
-                    },
-                },
-            ],
-
-            // For Monday draggable columns (optional)
-            cmpsOrder: ['StatusPicker', 'MemberPicker', 'DatePicker'],
+            isLocked: false
         }
     ]
-    saveToStorage(STORAGE_KEY, boards)
+    saveToStorage(STORAGE_KEY, wallets)
 }
