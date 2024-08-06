2f6120cf3afb6c803b32054c31ba8eb8e808f26b
Yonatan Hershko <yonher8@gmail.com>
2024-08-06 20:10:43 +0300

    Add money style

--- src/cmps/WalletPreview.jsx
+++ src/cmps/WalletPreview.jsx
@@ -7,11 +7,13 @@ export function WalletPreview({ wallet }) {
 
     return (
         <section className="wallet-preview ">
-
-            <div className="wallet-balance">
-                <h1>{wallet.title}</h1>
-                <h2>{wallet.balance} </h2>
+            <div className="wallet-balance-container" >
+                <div className="wallet-balance">
+                    <h1>{wallet.title}</h1>
+                    <h2>{wallet.balance} </h2>
+                </div>
             </div>
+
             <p>{wallet.description}</p>
             <img className="user-img" src={generateRobohashUrl(member._id)} alt={member.name} style={{ width: '100px', height: '100px' }} />
         </section>
