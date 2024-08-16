a1069a730214015ecf616454bf7e9177b5d0410a
Yonatan Hershko <yonher8@gmail.com>
2024-08-16 11:07:06 +0300

    Style wallet-balance

--- src/cmps/WalletPreview.jsx
+++ src/cmps/WalletPreview.jsx
@@ -1,28 +1,37 @@
 import { MoneyTransfers } from "./MoneyTransfers"
 import { WalletDetails } from "./WalletDetails"
+import walletBgImage from '../../src/assets/styles/imgs/wallet-index/image.png';
 
 export function WalletPreview({ wallet }) {
 
     const member = wallet.members
-    
+
+    // console.log(wallet);
 
 
     return (
         <section className="wallet-preview ">
             <div className="wallet-balance-container" >
-                <div className="wallet-balance">
-                    <h2><span>{wallet.currency}</span>{wallet.balance}  </h2>
-                    {/* <p>{wallet.title}</p> */}
+                <h3>{wallet.title}</h3>
+                <p>Hello {member[0].name}</p>{/* by login user */}
+
+                <div className="wallet-balance-bg">
+                    <img className="wallet-bg-img"
+                        src={walletBgImage} alt="Wallet" />
+                    <div className="wallet-balance">
+                        <h3 >יתרה כוללת</h3 >
+                        <h2><span>{wallet.currency}</span>{wallet.balance}  </h2>
+                    </div>
                 </div>
             </div>
             {/* <p>{wallet.description} or dashboard</p> */}
-
+            <hr className="bottom-line" />
             <WalletDetails
                 wallet={wallet}
             />
 
             <MoneyTransfers
-             wallet={wallet} />
+                wallet={wallet} />
 
         </section>
     )
