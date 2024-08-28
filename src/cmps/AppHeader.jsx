aaa90d8ab004b68898983f540d3683623e0da37f
Yonatan Hershko <yonher8@gmail.com>
2024-08-28 11:11:33 +0300

    Add home page

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,9 +1,30 @@
 import { Link, NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+import { useState } from 'react'
 
 export function AppHeader() {
 	const navigate = useNavigate()
+	const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useState(false)
+	const [isHamburgerMenuAnimating, setIsHamburgerMenuAnimating] = useState(false)
+
+
+	function toggleHamburgerMenu() {
+		if (isHamburgerMenuAnimating) return
+
+		setIsHamburgerMenuAnimating(true)
+		setIsHamburgerMenuOpen(!isHamburgerMenuOpen)
+
+		if (!isHamburgerMenuOpen) {
+			document.body.classList.add('no-scroll')
+		} else {
+			document.body.classList.remove('no-scroll')
+		}
+
+		setTimeout(() => {
+			setIsHamburgerMenuAnimating(false)
+		}, 500)
+	}
 
 	return (
 		<header className="app-header main-layout ">
@@ -15,6 +36,14 @@ export function AppHeader() {
 					<NavLink to="wallet" className="logo">
 						My Wallet
 					</NavLink>
+					<article className="hamburger-container" onClick={toggleHamburgerMenu}>
+						<div className={`hamburger ${isHamburgerMenuOpen ? 'is-open' : ''}`}>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</article>
+
 
 				</nav>
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,60 @@
+
+import React, { useEffect, useRef, useState } from 'react'
+import { ChevronRight } from 'lucide-react'
+import img1 from '../assets/styles/imgs/icones/icon2.jpg'
+import img2 from '../assets/styles/imgs/icones/girl2.jpg'
+import img3 from '../assets/styles/imgs/icones/robot3.jpg'
+
 export function HomePage() {
-    return (
-        <section className="">
-            <h1>Manage your wallet</h1>
-        </section >
-    )
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const images = [
+    { id: 1, url: img1, alt: '1' },
+    { id: 2, url: img2, alt: '2' },
+    { id: 3, url: img3, alt: '3' },
+  ]
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+    }, 3000)
+
+    return () => clearInterval(interval)
+  }, [images.length])
+
+  const getImageIndex = (index) => {
+    return (index + images.length) % images.length
+  }
+
+  return (
+    <div className="home-page">
+      <h1>Manage your Wallet</h1>
+      
+      <div className="gallery-container">
+        <div className="gallery">
+          {[-1, 0, 1].map((offset) => {
+            const index = getImageIndex(currentIndex + offset)
+            return (
+              <div 
+                key={images[index].id} 
+                className={`main-imgs ${offset === 0 ? 'center' : ''}`}
+              >
+                <img src={images[index].url} alt={images[index].alt} />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div>
+      <button className="create-wallet-btn">
+        Create new wallet
+        <ChevronRight />
+      </button>  
+      <a href="#" className="existing-wallet-link">
+        I already have a wallet
+      </a>
+
+      </div>
+    </div>
+  )
 }
 
